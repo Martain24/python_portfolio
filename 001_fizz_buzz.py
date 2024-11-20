529e903c844dc33b4ffc8c4062d78a3d2547d8dc
Martain24 <vilaboamartin@gmail.com>
2024-11-20 15:29:42 +0100

    cambios en estructura

---
+++ 001_fizz_buzz.py
@@ -0,0 +1,15 @@
+# Fizz Buzz
+## Imprimir todos los enteros del 1 al 100.
+## Si el número es múltiplo de 3 mostramos Fizz.
+## Si el número es múltiplo de 5 mostramos Buzz.
+## Si el número es múltiplo de 3 y 5 mostramos FizzBuzz.
+
+
+
+
+
+
+
+
+
+
