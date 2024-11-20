849a36a67848577b9322b68a9cab4822f34fc1d8
Martain24 <vilaboamartin@gmail.com>
2024-11-20 15:51:57 +0100

    Video Fizz Buzz terminado

--- 001_fizz_buzz.py
+++ 001_fizz_buzz.py
@@ -4,6 +4,10 @@
 ## Si el número es múltiplo de 5 mostramos Buzz.
 ## Si el número es múltiplo de 3 y 5 mostramos FizzBuzz.
 
+for number in range(1, 101):
+    message = "Fizz" if (number % 3 == 0) else ""
+    message += "Buzz" if (number % 5 == 0) else ""
+    print(number if message == "" else message)
 
 
 
