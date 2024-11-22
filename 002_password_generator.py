57a44d5b4268e50325a2598722ac2737725adf0c
Martain24 <vilaboamartin@gmail.com>
2024-11-22 19:34:02 +0100

    no hace falta lista indexes

--- 002_password_generator.py
+++ 002_password_generator.py
@@ -1,47 +1,37 @@
-# Password generator
-## Preguntamos al usuario cuantas letras quiere que tenga la contraseña
-## Cuantos símbolos
-## Cuantos números
-## Generamos contraseña
-
-import string 
+import string
 import random
 
-alphabet_list = list(string.ascii_uppercase + string.ascii_lowercase)
-simbolos_list = list("#@_%&=+-[]^*")
-numeros_list = list("123456789")
+abecedario = string.ascii_lowercase + string.ascii_uppercase
+simbolos = "#@*%"
+numeros = "0123456789"
 
-def ask_for_number(input_str):
+def ask_for_number(question):
     while True:
         try:
-            number = int(input(input_str))
+            numero = int(input(question))
             break
         except:
-            print("No has escrito un número válido.")
-    return number
+            print("No has escrito un entero válido.")
+    
+    return numero
 
+num_letras = ask_for_number("¿Cuántas letras quieres que tenga la contraseña? ")
+num_simbolos = ask_for_number("¿Cuántos símbolos quieres que tenga la contraseña? ")
+num_numeros = ask_for_number("¿Cuántos números quieres que tenga la contraseña? ")
 
-num_letras = ask_for_number("¿Cuántas letras quieres que tenga tu contraseña? ")
-num_simbolos = ask_for_number("¿Cuántos símbolos quieres que tenga tu contraseña? ")
-num_numeros = ask_for_number("¿Cuántos números quieres que tenga tu contraseña? ")
-        
-        
-letras = random.choices(alphabet_list, k=num_letras)
-simbolos = random.choices(simbolos_list, k=num_simbolos)
-numeros = random.choices(numeros_list, k=num_numeros)
+letras_password = random.choices(abecedario, k=num_letras)
+simbolos_password = random.choices(simbolos, k=num_simbolos)
+numeros_password = random.choices(numeros, k=num_numeros)
 
-
-list_password = letras + simbolos + numeros
-# random.shuffle(list_password)
-indexes = [i for i in range(len(list_password))]
-
+list_password = letras_password + simbolos_password + numeros_password
 password = ""
 
-while True:
-    random_index = random.choice(indexes)
-    password += list_password[random_index]
-    indexes.remove(random_index)
-    if len(indexes) == 0:
-        break
+while len(list_password) > 0:
+    random_element = random.choice(list_password)
+    password += random_element
+    list_password.remove(random_element)
 
-print(password)
+print(f"Tu contraseña es: {password}")
+
+
+
